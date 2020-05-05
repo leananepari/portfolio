f1469b5d12996f8bcdeb7f435176f4f0d7df22e0
Leana N <leananeparidze@gmail.com>
2020-05-04 18:03:48 -0700

    adds most recent project

--- src/components/mainContent/ProjectList.js
+++ src/components/mainContent/ProjectList.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavLink as Link } from 'react-router-dom';
 import { projects } from '../../data';
 import ProjectCard from './ProjectCard';
-import { Player } from 'video-react';
+import video from '../../assets/to-do-video.mp4';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub} from '@fortawesome/free-brands-svg-icons';
 
 const ProjectList = () => {
   
+  useEffect(() => {
+    document.querySelector('video').playbackRate = 2;
+  })
   return (
- 
-    <div id='projects'>
-      {/* {projects.map((project) => {
-        return <ProjectCard project={project} key={project.name}/>
-      })} */}
-      <p style={{margin: '100px'}}>Updating project list</p>
+    // PREVIOUS PROJECT LIST
+    // <div id='projects'>
+    //   {/* {projects.map((project) => {
+    //     return <ProjectCard project={project} key={project.name}/>
+    //   })} */}
+    //   <p style={{margin: '100px'}}>Updating project list</p>
+    // </div>
+
+    <div className="content" style={{marginTop: '120px'}}>
+      <div className="project-description">
+        <div style={{display: 'flex'}}>
+          <h1>Task Manager App</h1>
+          <Link to="" target="_blank" onClick={(event) => {event.preventDefault(); window.open('https://github.com/leananepari/to-do-app');}} className="icon"> 
+            <FontAwesomeIcon style={{width: '20px', cursor: 'pointer', color: '#2097D1', marginRight: '10px'}} icon={faGithub} size='lg'/> 
+          </Link>
+        </div>
+        <div>
+          Currently working on building a full-stack application that lets you add tasks, set due dates, get reminders, and customize themes.
+        </div>
+        <div className="tech">
+          <h2>Front End:</h2>
+          <p>React</p>
+          <h2>Back End:</h2>
+          <p>Spring</p>
+        </div>
+      </div>
+      <video loop autoPlay muted={true} >
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 };
