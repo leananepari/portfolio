fa6b6abf83077fb6f40c112e7fecab5a8d8fbc4d
Leana N <leananeparidze@gmail.com>
2020-06-29 14:42:25 -0700

    adds mobile view menu dropdown

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAt } from '@fortawesome/free-solid-svg-icons';
@@ -6,14 +6,60 @@ import { faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
 
 
 const Navbar = () => {
+  const [width, setWidth] = useState();
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+
+    setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+  }, [])
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    if (window.innerWidth > 500) {
+      setShowMenu(false);
+    }
+  }
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  }
 
   return (
     <div className="navbar">
       <div className="links">
-        <Link exact to="/" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>HOME</Link>
-        <Link to="/projects" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>PROJECTS</Link>
-        <Link to="/about" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>ABOUT</Link>
+        {width <= 500 ? 
+          <div>
+            <svg viewBox="0 0 100 60" width="40" height="40" onClick={handleMenuClick} style={{cursor: 'pointer'}} 
+                 >
+              <rect width="90" height="11" rx="8" fill="white"></rect>
+              <rect y="25" width="90" height="11" rx="8" fill="white"></rect>
+              <rect y="50" width="90" height="11" rx="8" fill="white"></rect>
+            </svg>
+            {showMenu ? 
+              <div className="menu-wrap">
+                <Link exact to="/" className="menu-linkReg">HOME</Link>
+                <Link to="/projects" className="menu-linkReg">PROJECTS</Link>
+                <Link to="/about" className="menu-linkReg">ABOUT</Link>
+              </div>
+            :
+            null
+            }
+          </div>
+        : 
+        width > 500 ?
+          <div>
+            <Link exact to="/" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>HOME</Link>
+            <Link to="/projects" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>PROJECTS</Link>
+            <Link to="/about" className="linkReg" activeStyle={{ borderBottom: '1px solid white' }}>ABOUT</Link>
+          </div>
+        : 
+          null
+        }
       </div>
+
       <div className="icons">
 
         <Link to="/emailme" className="icon">
